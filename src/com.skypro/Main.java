8809c40456eb882b03e6b36906f4d4497fb2149b
JohanNayvert <111690307+JohanNayvert@users.noreply.github.com>
2022-11-02 19:37:40 +0500

    First Commit

---
+++ src/com.skypro/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("TaskForPractic");
+        System.out.println();
+
+
+
+        System.out.println("HomeWork Complete");
+        System.out.println();
+    }
+}
